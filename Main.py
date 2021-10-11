31b154637060579c9ad3a821a4f8778ea5e5ff5b
Thecodebilly <64550116+Thecodebilly@users.noreply.github.com>
2021-10-10 23:15:07 -0400

    Update Main.py
    finished data manipulation

--- Main.py
+++ Main.py
@@ -1,17 +1,14 @@
 import csv
+import numpy
 from sklearn import svm
 
 
 def SVMprediction():
     filename = "outputForSVM.csv"
     rowcount = 0
-    colcount = 0
-
-    # amount of columns in csv
-    cols = 100
 
     arrayoflocations = []
-    arr = [[],[]]
+    arr = []
 
     # reading csv file
     with open(filename, 'r') as csvfile:
@@ -24,77 +21,85 @@ def SVMprediction():
         firstrow = True
         for row in csvreader:
             sameID = True
-            #print(row)
-            #print("again")
             if firstrow:
-                #print(row[0])
                 NewLocID = row[0]
-                LocID= row[0]
-                firstrow=False
+                LocID = row[0]
+                firstrow = False
 
 
             else:
-                NewLocID= row[0]
-           #print("new")
-           # print(NewLocID)
-            #print("old")
-            #print(LocID)
+                NewLocID = row[0]
+
             if LocID != NewLocID:
                 LocID = NewLocID
                 sameID = False
 
             #print(sameID)
             if sameID is False:
-                #print(arr)
-                # push 2d array to an array,
-                # print(arr)
-                arrayoflocations.append(arr)
-
+                # transpose and push location 2d array
+                rez = [[arr[j][i] for j in range(len(arr))] for i in range(len(arr[0]))]
+                arrayoflocations.append(rez)
+                #print(arrayoflocations)
                 # and clear the current 2d array
+                arr = []
 
-                arr = [[],[]]
-                #print(arr)
 
-                #print(arr)
-               #print("\n\nin")
-
-                # reset row iterator
                 rowcount = 0
 
-            # reset column iterator
-            colcount = 0
             temp = []
-            firstcol=True
+            firstcol = True
+
             for col in row:
                 #print(col)
                 if firstcol is True:
-                    firstcol=False
-                    #NewLocID = col
-                    #firstcol = False
+                    firstcol = False
+                    continue
                 else:
-                    # make a 2d array that will contain the rssi readings
-                    # print(int(col))
-
                     temp.append(int(col))
-                    arr.append(temp)
+            arr.append(temp)
 
-                colcount += 1
-            #print(colcount)
+
+
+
             rowcount += 1
-            #print(rowcount)
+            # print(rowcount)
+
+    #print(arrayoflocations)
 
 
-    print(arrayoflocations)
+    #formatting of cvs above
+    ###################################
+    #calling SVM below
+
+    print(arrayoflocations[0][0])
+    print(arrayoflocations[0][1])
+    print(arrayoflocations[0][2])
+
+    beacons3loc1=[[1,arrayoflocations[0][0]],[2,arrayoflocations[0][1]],[3,arrayoflocations[0][2]]]
+    X = beacons3loc1
+
+    #location1
+    #106,79
+    #beacon1
+    #43,88
+    #beacon2
+    #68,88
+    #beacon3
+    #103,74
 
 
-# X = [[0, 0], [2, 2]]
+    #euclidean dist1
+    #63.6396
+    #euclidean dist2
+    #39.05125
+    #euclidean dist3
+    #5.83095
 
-# multi-label
-# beacon coords
-# y = [0.5, 2.5]
 
-# regr = svm.SVR()
-# add in kernel
-# regr.fit(X, y)
-# regr.predict([[1, 1]])
-SVMprediction()
+    y = [63.6396, 39.05125,5.83095]
+
+    regr = svm.SVR()
+    # add in kernel
+    regr.fit(X, y)
+    #print(regr.predict([[1, arrayoflocations[1][0]]]))
+
