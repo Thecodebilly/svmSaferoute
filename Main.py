f0eadfb62f79c8221c478380f05c2537fc6b4d5a
Billy Shaw <64550116+Thecodebilly@users.noreply.github.com>
2021-11-07 11:42:14 -0500

    Update Main.py
    got a model with lots of samples included. Need to tweak parameters.

--- Main.py
+++ Main.py
@@ -1,13 +1,39 @@
 import csv
+import math
+
 import numpy as np
 from sklearn import svm
+import mysql.connector
+
+#instead of dictionary... use array of beacon IDs
+NonusedBeacons=[]
+activeBeaconsCoords=[]
+LocationCoords=[]
+
+def getBeaconLocCoords():
+
+    with open("beacons.csv", 'r') as csvfile:
+        # creating a csv reader object
+        csvreader2 = csv.reader(csvfile)
+        for row in csvreader2:
+                activeBeaconsCoords.append([int(row[0]),int(row[3]),int(row[4])])
+
+def getLocationCoords():
+
+    with open("id_to_coords.csv", 'r') as csvfile:
+        # creating a csv reader object
+
+        csvreader3 = csv.reader(csvfile)
+        next(csvreader3)
+        for row in csvreader3:
+                LocationCoords.append([int(row[0]),int(row[1]),int(row[2])])
 
 
-def getBeaconsFromSQL():
-    print()
 
 
 def SVMprediction():
+    getBeaconLocCoords()
+    getLocationCoords()
     filename = "outputForSVM.csv"
     rowcount = 0
 
@@ -64,24 +90,34 @@ def SVMprediction():
             rowcount += 1
             # print(rowcount)
 
-    # print(arrayoflocations)
+    #print(arrayoflocations)
 
     # formatting of cvs above
     ###################################
     # calling SVM below
 
     X = []
+    y = []
+    ReadingLocID=1
     for i in arrayoflocations:
-        id = 0
+        BeaconLocID = 1
+        #print(i)
+        #update Location ID
         for j in i:
             maxreading = max(j)
             if maxreading != -1000:
-                X.append((id, maxreading))
-            id += 1
+                X.append((BeaconLocID, maxreading))
+                euclideandist= math.dist((LocationCoords[ReadingLocID][1],LocationCoords[ReadingLocID][2]),(activeBeaconsCoords[BeaconLocID][1],activeBeaconsCoords[BeaconLocID][2]))
+                y.append(euclideandist)
 
-    # print(X)
+            BeaconLocID += 1
+        ReadingLocID+=1
 
-    X = [(1, -79), (2, -82), (3, -29)]
+
+
+    print(X)
+    print(y)
+    #X = [(1, -79), (2, -82), (3, -29)]
     # this seemingly works-> X = [(1, -79), (2, -82), (3, -29)]
     # X= beacons3loc1
     # location1
@@ -99,17 +135,9 @@ def SVMprediction():
     # 39.05125
     # euclidean dist3
     # 5.83095
-    filename = "id_to_coords.csv"
-    IDdictionary = {}
-    with open(filename, 'r') as csvfile:
-        # creating a csv reader object
-        csvreader = csv.reader(csvfile)
-        # skip header
-        next(csvreader)
-        for row in csvreader:
-            IDdictionary[row[0]] = (row[1], row[2])
 
-    y = [63.6396, 39.05125, 5.83095]
+
+    #y = [63.6396, 39.05125, 5.83095]
 
     regr = svm.SVR(kernel="rbf")
 
@@ -117,7 +145,7 @@ def SVMprediction():
 
     # print("beacon1, location2... used for prediction: ")
     # print(arrayoflocations[1][0])
-    arrr = [1, -78]
+    arrr = [1, -79]
     print(regr.predict(np.array(arrr).reshape(1, -1)))
 
 
