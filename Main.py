7f35ca996c127cf53475d0c981859eaca393af1e
Billy Shaw <64550116+Thecodebilly@users.noreply.github.com>
2021-11-29 20:23:46 -0500

    Update Main.py
    added pickle

--- Main.py
+++ Main.py
@@ -1,34 +1,38 @@
+#!/usr/local/bin/python3
+import nltk
+import sys
+
+# input = sys.argv[1]
+import pickle
 import csv
 import math
 
 import numpy as np
 from sklearn import svm
-import mysql.connector
 
-#instead of dictionary... use array of beacon IDs
-NonusedBeacons=[]
-activeBeaconsCoords=[]
-LocationCoords=[]
+regr = svm.SVR(kernel="rbf")
+# instead of dictionary... use array of beacon IDs
+NonusedBeacons = []
+activeBeaconsCoords = []
+LocationCoords = []
+
 
 def getBeaconLocCoords():
-
     with open("beacons.csv", 'r') as csvfile:
         # creating a csv reader object
         csvreader2 = csv.reader(csvfile)
         for row in csvreader2:
-                activeBeaconsCoords.append([int(row[0]),int(row[3]),int(row[4])])
+            activeBeaconsCoords.append([int(row[0]), int(row[3]), int(row[4])])
+
 
 def getLocationCoords():
-
     with open("id_to_coords.csv", 'r') as csvfile:
         # creating a csv reader object
 
         csvreader3 = csv.reader(csvfile)
         next(csvreader3)
         for row in csvreader3:
-                LocationCoords.append([int(row[0]),int(row[1]),int(row[2])])
-
-
+            LocationCoords.append([int(row[0]), (int(row[1]), int(row[2]))])
 
 
 def SVMprediction():
@@ -90,63 +94,47 @@ def SVMprediction():
             rowcount += 1
             # print(rowcount)
 
-    #print(arrayoflocations)
+    # print(arrayoflocations)
 
-    # formatting of cvs above
+    # Formatting of csv above
     ###################################
     # calling SVM below
 
     X = []
     y = []
-    ReadingLocID=1
+    ReadingLocID = 1
     for i in arrayoflocations:
         BeaconLocID = 1
-        #print(i)
-        #update Location ID
+        # update Location ID
         for j in i:
             maxreading = max(j)
             if maxreading != -1000:
-                X.append((BeaconLocID, maxreading))
-                euclideandist= math.dist((LocationCoords[ReadingLocID][1],LocationCoords[ReadingLocID][2]),(activeBeaconsCoords[BeaconLocID][1],activeBeaconsCoords[BeaconLocID][2]))
-                y.append(euclideandist)
+                if BeaconLocID == 2:
+                    X.append((BeaconLocID, maxreading))
+                    euclideandist = math.dist((LocationCoords[ReadingLocID][1][0], LocationCoords[ReadingLocID][1][1]),
+                                              (
+                                                  activeBeaconsCoords[BeaconLocID][1],
+                                                  activeBeaconsCoords[BeaconLocID][2]))
+                    y.append(euclideandist)
 
             BeaconLocID += 1
-        ReadingLocID+=1
-
-
+        ReadingLocID += 1
 
     print(X)
     print(y)
-    #X = [(1, -79), (2, -82), (3, -29)]
-    # this seemingly works-> X = [(1, -79), (2, -82), (3, -29)]
-    # X= beacons3loc1
-    # location1
-    # 106,79
-    # beacon1
-    # 43,88
-    # beacon2
-    # 68,88
-    # beacon3
-    # 103,74
 
-    # euclidean dist1
-    # 63.6396
-    # euclidean dist2
-    # 39.05125
-    # euclidean dist3
-    # 5.83095
-
-
-    #y = [63.6396, 39.05125, 5.83095]
-
-    regr = svm.SVR(kernel="rbf")
-
+    regr.C = 1000
+    regr.gamma = 1
     regr.fit(X, y)
 
-    # print("beacon1, location2... used for prediction: ")
-    # print(arrayoflocations[1][0])
-    arrr = [1, -79]
-    print(regr.predict(np.array(arrr).reshape(1, -1)))
+    # print(regr.predict(np.array([1,-79]).reshape(1, -1)))
+
+
+def SaveSVM(model):
+    with open('SVM.pkl', 'wb') as file:
+        pickle.dump(model, file)
 
 
 SVMprediction()
+SaveSVM(regr)
+
