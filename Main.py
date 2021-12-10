cc8efc2bcdba5bda7ffbd7e015179dc678c96c1c
Billy Shaw <64550116+Thecodebilly@users.noreply.github.com>
2021-12-09 22:49:20 -0500

    Update Main.py
    now with graphs and comments

--- Main.py
+++ Main.py
@@ -1,22 +1,27 @@
-#!/usr/local/bin/python3
-import nltk
-import sys
-
-# input = sys.argv[1]
 import pickle
 import csv
 import math
-
+import numpy
 import numpy as np
 from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
 
-regr = svm.SVR(kernel="rbf")
 # instead of dictionary... use array of beacon IDs
 NonusedBeacons = []
 activeBeaconsCoords = []
 LocationCoords = []
+X = []
+y = []
 
 
+# R^2 method to quantify the model's accuracy, adjusts for overfitting
+def AdjustedRSquared(RSquared, n, k):
+    return 1 - ((1 - RSquared) * (n - 1) / (n - k - 1))
+
+
+# gets the beacon coordinates for the Beacons table in the myPHPAdmin
 def getBeaconLocCoords():
     with open("beacons.csv", 'r') as csvfile:
         # creating a csv reader object
@@ -25,6 +30,7 @@ def getBeaconLocCoords():
             activeBeaconsCoords.append([int(row[0]), int(row[3]), int(row[4])])
 
 
+# gets the location coordinates from the Java data cleaner file
 def getLocationCoords():
     with open("id_to_coords.csv", 'r') as csvfile:
         # creating a csv reader object
@@ -35,6 +41,12 @@ def getLocationCoords():
             LocationCoords.append([int(row[0]), (int(row[1]), int(row[2]))])
 
 
+def SaveSVM(model):
+    with open('SVM.pkl', 'wb') as file:
+        pickle.dump(model, file)
+
+
+# populates the dependent and independent variables to be input into the SVM
 def SVMprediction():
     getBeaconLocCoords()
     getLocationCoords()
@@ -44,7 +56,7 @@ def SVMprediction():
     arrayoflocations = []
     arr = []
 
-    # reading csv file
+    # reading main output from Java cleaner (outputForSVM)
     with open(filename, 'r') as csvfile:
         # creating a csv reader object
         csvreader = csv.reader(csvfile)
@@ -52,6 +64,7 @@ def SVMprediction():
         # skip header
         next(csvreader)
 
+        # getting first Location ID
         firstrow = True
         for row in csvreader:
             sameID = True
@@ -60,102 +73,135 @@ def SVMprediction():
                 LocID = row[0]
                 firstrow = False
 
-
+            # record Location ID for current row
             else:
                 NewLocID = row[0]
-
+            # if the location ID changed from last row, flag that the ID has changed
             if LocID != NewLocID:
                 LocID = NewLocID
                 sameID = False
 
-            # print(sameID)
+            # act on flag, store all data for this location ID
             if sameID is False:
                 # transpose and push location 2d array
+                # this is to store all the columns of data for a given beacon at a location in an array
+                # each row contains all of the readings for a beacon at a location ID
                 rez = [[arr[j][i] for j in range(len(arr))] for i in range(len(arr[0]))]
                 arrayoflocations.append(rez)
-                # print(arrayoflocations)
                 # and clear the current 2d array
                 arr = []
 
                 rowcount = 0
 
+            # temp is to store RSSIs
             temp = []
+
             firstcol = True
 
             for col in row:
-                # print(col)
+                # now we are getting RSSI readings from each row, so skip the first index (the location ID)
                 if firstcol is True:
                     firstcol = False
                     continue
                 else:
+                    # append the RSSI as an int
                     temp.append(int(col))
+            # append the row of RSSIs for the current row in the current location ID to the array for the whole location ID
             arr.append(temp)
 
+            # ++1
             rowcount += 1
-            # print(rowcount)
-
-    # print(arrayoflocations)
 
     # Formatting of csv above
     ###################################
     # calling SVM below
 
-    X = []
-    y = []
     ReadingLocID = 0
+    # for the column of data in a given location for a beacon
     for i in arrayoflocations:
         BeaconLocID = 0
         # update Location ID
+        # for value in data column
         for j in i:
+            # get best RSSI in data column (because highest RSSI is most accurate)
             maxreading = max(j)
+            # if not placeholder value
             if maxreading != -1000:
-                #if BeaconLocID == 2:
-                    X.append((BeaconLocID, maxreading))
-                   # print(ReadingLocID, BeaconLocID,  maxreading)
-                    euclideandist = math.dist([LocationCoords[ReadingLocID][1][0], LocationCoords[ReadingLocID][1][1]],
-                                              [
-                                                  activeBeaconsCoords[BeaconLocID][1],
-                                                  activeBeaconsCoords[BeaconLocID][2]])
-                    y.append(euclideandist)
-                   # print("euclidean dist:")
-                    #print(euclideandist)
-                    #print("x1 ")
-                    #print(LocationCoords[ReadingLocID][1][0])
-                    #print("y1 ")
-                    #print(LocationCoords[ReadingLocID][1][1])
-                    #print("x2 " )
-                    #print(activeBeaconsCoords[BeaconLocID][1])
-                    #print("y2 ")
-                    #print(activeBeaconsCoords[BeaconLocID][2])
+                # add dependant variable
+                X.append((BeaconLocID, maxreading))
 
-            BeaconLocID+=1
+                # add independent value
+                euclideandist = math.dist([LocationCoords[ReadingLocID][1][0], LocationCoords[ReadingLocID][1][1]],
+                                          [
+                                              activeBeaconsCoords[BeaconLocID][1],
+                                              activeBeaconsCoords[BeaconLocID][2]])
+                y.append(euclideandist)
 
-        ReadingLocID+= 1
+                # we are using the tuple of (BeaconLocID, maxreading) to predict the distance
+                # from a beacon to a user who got the RSSI reading
+
+            BeaconLocID += 1
+
+        ReadingLocID += 1
 
 
-
-
-
-
-
-    print(X)
-    print(y)
-
-    #print("location table")
-    #print(LocationCoords)
-    #print("beacons table")
-    #print(activeBeaconsCoords)
-    regr.C = 10000000000
-    regr.gamma = 1
-    regr.fit(X, y)
-
-    print(regr.predict(np.array([1,-45]).reshape(1, -1)))
-
-
-def SaveSVM(model):
-    with open('SVM.pkl', 'wb') as file:
-        pickle.dump(model, file)
-
-
+# get X,y
 SVMprediction()
-SaveSVM(regr)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.001, random_state=1)
+
+algArr = []
+n = 2.25
+for xval in X_test:
+    exp = (-65 - xval[1]) / (10 * n)
+    dist = 3.28 * math.pow(10, exp)
+    algArr.append(dist)
+
+c = 1
+rmse_svm = []
+y_true = y_test
+for i in range(5):
+    regr = svm.SVR(kernel="rbf")
+    regr.C = c
+    regr.fit(X_train, y_train)
+    y_pred = regr.predict(X_test)
+    print(regr.score(X_test, y_test))
+    print(AdjustedRSquared(regr.score(X_test, y_test), len(X_train), 34))
+    rmse_svm.append(math.sqrt(mean_squared_error(y_true, y_pred)))
+    c = c * 10
+
+rmse_alg = math.sqrt(mean_squared_error(y_true, algArr))
+
+# penalty
+
+# regr.gamma =1
+
+# regr.degree
+
+# regr.coef0
+
+# formulaic/algorithmic approach ( for comparison )
+
+
+# print(sum(abs(numpy.subtract(numpy.array(y_true), numpy.array(y_pred)))))
+
+
+print(rmse_svm)
+# SaveSVM(regr)
+
+data = {'SVM, C=1': rmse_svm[0], 'SVM, C=10': rmse_svm[1], 'SVM, C=100': rmse_svm[2], 'SVM, C=1000': rmse_svm[3],
+        'SVM, C=10000': rmse_svm[4], 'formula': rmse_alg}
+courses = list(data.keys())
+values = list(data.values())
+
+fig = plt.figure(figsize=(10, 5))
+
+# creating the bar plot
+plt.bar(courses, values, color='maroon',
+        width=0.4)
+
+plt.xlabel("Model Used")
+plt.ylabel("Root Mean Squared Error (RMSE)")
+plt.title("RMSE of RSSI-to-Distance Methods")
+plt.show()
